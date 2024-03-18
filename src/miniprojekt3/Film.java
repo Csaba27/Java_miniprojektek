5047fe29dbf1afe8362a2b3cba8f968abf695adf
Csaba27 <benedekcsaba43@gmail.com>
2024-03-18 16:47:35 +0200

    Miniprojekt3 add velemenyekSzama function

--- src/miniprojekt3/Film.java
+++ src/miniprojekt3/Film.java
@@ -4,12 +4,17 @@ public class Film {
     protected String cim;
     protected int ar;
     protected int[] velemenyek;
-    private int velemenyekSzama = 0;
+
+    {
+        velemenyek = new int[20];
+        for (int i = 0; i < velemenyek.length; i++) {
+            velemenyek[i] = -1;
+        }
+    }
 
     public Film(String cim, int ar) {
         this.cim = cim;
         this.ar = ar;
-        velemenyek = new int[20];
     }
 
     public String getCim() {
@@ -26,17 +31,29 @@ public class Film {
 
     public void Velemenyezes(int velemeny) {
         if (velemeny >= 1 && velemeny <= 5) {
-            velemenyek[velemenyekSzama++] = velemeny;
+            velemenyek[velemenyekSzama()] = velemeny;
         }
     }
 
+    public int velemenyekSzama() {
+        int num = 0;
+        for (int i : velemenyek) {
+            if (i == -1) {
+                break;
+            } else {
+                num++;
+            }
+        }
+        return num;
+    }
+
     public double Atlag() {
-        if (velemenyekSzama > 0) {
+        if (velemenyekSzama() > 0) {
             int osszeg = 0;
-            for (int velemeny: velemenyek) {
+            for (int velemeny : velemenyek) {
                 osszeg += velemeny;
             }
-            return (double) osszeg / velemenyekSzama;
+            return (double) osszeg / velemenyekSzama();
         }
         System.out.println("Ez a film még nem kapott véleményt!");
         return 0.0;
